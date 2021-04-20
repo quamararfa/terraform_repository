c0b27836b408f70ed6896d6cd4100c1db29fca62
quamararfa <50367361+quamararfa@users.noreply.github.com>
2021-04-20 20:19:02 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -11,3 +11,17 @@ resource "aws_subnet" "mysubnet" {
 resource "aws_internet_gateway" "mygw" {
   vpc_id = aws_vpc.myvpc.id
 }
+
+resource "aws_route_table" "myroutetable" {
+  vpc_id = aws_vpc.myvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.mygw.id
+    }
+  }
+
+resource "aws_route_table_association" "routeassociation" {
+  subnet_id      = aws_subnet.mysubnet.id
+  route_table_id = aws_route_table.myroutetable.id
+}
