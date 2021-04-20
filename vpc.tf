34c0ccb61e119900874812a991c855910b22abe1
quamararfa <50367361+quamararfa@users.noreply.github.com>
2021-04-20 20:00:36 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -3,8 +3,9 @@ resource "aws_vpc" "myvpc" {
 }
 
 resource "aws_subnet" "mysubnet" {
-  vpc_id     = aws_vpc.myvpc.id
+ vpc_id     = aws_vpc.myvpc.id
  cidr_block = "10.0.1.0/24"
+ map_public_ip_on_launch = "true"  
 }
 
 resource "aws_internet_gateway" "mygw" {
