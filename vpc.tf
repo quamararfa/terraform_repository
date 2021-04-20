8854c1a32259a7fe97146d721f2a77c580585785
quamararfa <50367361+quamararfa@users.noreply.github.com>
2021-04-20 17:49:45 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,8 @@
 resource "aws_vpc" "myvpc" {
-    cidr_block = "10.0.0.0/16"
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "mysubnet" {
+  vpc_id     = aws_vpc.myvpc.id
+ cidr_block = "10.0.1.0/24"
 }
